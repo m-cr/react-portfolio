fe36fe75fcca4b73c4b1bf066d432fd6dd581efa
Michael Crowley <miketcrowley@gmail.com>
2017-01-13 14:24:12 -0500

    keep heroku awake

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, 'index.html')));
 
+var http = require('http');
+
+setInterval(function() {
+	console.log('hitting');
+	http.get('http://michaeltcrowley.net');
+}, 300000);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, ()=> console.log(`listening on port: ${port}`));
